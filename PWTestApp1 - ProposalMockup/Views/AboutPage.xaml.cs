c55fde638569e9c9b44572e72e2654d4a81500e8
OS doge <ngyuheng08@outlook.com>
2021-08-04 19:06:24 +0800

    Updates: Did more work and managed to get Announcement Buttons to work somewgat

--- PWTestApp1 - ProposalMockup/Views/AboutPage.xaml.cs
+++ PWTestApp1 - ProposalMockup/Views/AboutPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -7,7 +9,8 @@ namespace PWTestApp1___ProposalMockup.Views
 {
     public partial class AboutPage : ContentPage
     {
-
+        public static string announcements;
+        public static List<string> announcementList = new List<string>();
         public AboutPage()
         {
             StackLayout stackLayout = Announcements;
@@ -16,7 +19,6 @@ namespace PWTestApp1___ProposalMockup.Views
 
         public void countButton(System.Object sender, System.EventArgs e)
         {
-
 
             //int c = int.Parse(buttonNum.Text);
             //for(int i = 0; i <= c; i++)
@@ -57,14 +59,24 @@ namespace PWTestApp1___ProposalMockup.Views
         }
         private void buttontoinsertmoreannouncementsthisisthelongestnameever_Clicked(object sender, EventArgs e)
         {
-            Console.WriteLine("Its working i think");
+            announcements = buttonNum.Text;
+            announcementList.Add(announcements);
+            int i = announcementList.Count();
             Announcements.Children.Clear();
-            int x = Convert.ToInt32(buttonNum.Text);
-            while (x > 0)
+            for (int d = 0; d < i; d++)
             {
-                Announcements.Children.Add(new Button { Text = "Announcement" });
-                x--;
+                Console.WriteLine(announcementList[d]);
+                Announcements.Children.Add(new Button { Text = announcementList[d] });
             }
+            
+            //Console.WriteLine("Its working i think");
+            //Announcements.Children.Clear();
+            //int x = Convert.ToInt32(buttonNum.Text);
+            //while (x > 0)
+            //{
+            //    Announcements.Children.Add(new Button { Text = "Announcement" });
+            //    x--;
+            //}
         }
     }
 }
