f941d6f97552ba41361ec4b46ba96e020f08a4bb
OS doge <ngyuheng08@outlook.com>
2021-08-07 22:30:21 +0800

    QR code working?

--- PWTestApp1 - ProposalMockup/Views/safeEntry.xaml.cs
+++ PWTestApp1 - ProposalMockup/Views/safeEntry.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZXing.Net.Mobile.Forms;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -15,6 +16,21 @@ namespace PWTestApp1___ProposalMockup.Views
         public safeEntry()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Pressed(object sender, EventArgs e)
+        {
+            var scan = new ZXingScannerPage();
+            await Navigation.PushAsync(scan);
+            scan.OnScanResult += (result) =>
+              {
+                  Device.BeginInvokeOnMainThread(async () =>
+                  {
+                      await Navigation.PopAsync();
+                      QRData.Text = result.Text;
+                  }
+                      );
+              };
         }
     }
 }
