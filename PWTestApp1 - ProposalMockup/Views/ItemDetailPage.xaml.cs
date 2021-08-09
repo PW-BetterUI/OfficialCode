3112eb84cc3388025b176254bcefeb4a509fe133
OS doge <ngyuheng08@outlook.com>
2021-08-09 16:28:20 +0800

    Saved Announcements progress

--- PWTestApp1 - ProposalMockup/Views/ItemDetailPage.xaml.cs
+++ PWTestApp1 - ProposalMockup/Views/ItemDetailPage.xaml.cs
@@ -2,10 +2,19 @@
 using System.ComponentModel;
 using Xamarin.Forms;
 
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.IO;
+using System;
+using Xamarin.Essentials;
+using System.Linq;
+
 namespace PWTestApp1___ProposalMockup.Views
 {
     public partial class ItemDetailPage : ContentPage
     {
+        string localPath;
+        public static string title;
         public ItemDetailPage(string Title, string Content)
         {
             InitializeComponent();
@@ -17,7 +26,41 @@ namespace PWTestApp1___ProposalMockup.Views
 
         private void ToolbarItem_Clicked(object sender, System.EventArgs e)
         {
+            //string currentDir = Directory.GetCurrentDirectory();
 
+            //string titleFilepath = currentDir + @"\AnnouncementTitle";
+            //string contentFilepath = currentDir + @"\AnnouncementContent";
+
+            //Console.WriteLine(titleFilepath + "T/C " + contentFilepath);
+            //JsonSerialize(AnnouncementTitle.Text, titleFilepath);
+            //JsonSerialize(AnnouncementContent.Text, contentFilepath);
+            title = AnnouncementTitle.Text;
+            localPath = Path.Combine(FileSystem.AppDataDirectory , "AnnoucementTitle");
+            File.WriteAllText(localPath, AnnouncementTitle.Text);
+
+            //using (var stream = await FileSystem.OpenAppPackageFileAsync("AnnouncementTitle"))
+            //{
+            //    using (var reader = new StreamReader(stream))
+            //    {
+            //        AnnouncementTitle.Text = await reader.ReadToEndAsync();
+            //    }
+            //}
         }
+
+        //public void JsonSerialize(object data, string filepath)
+        //{
+        //    JsonSerializer jsonSerializer = new JsonSerializer();
+        //    if (File.Exists(filepath))
+        //    {
+        //        File.Delete(filepath);
+        //    }
+        //    StreamWriter sw = new StreamWriter(filepath);
+        //    JsonWriter jsonWriter = new JsonTextWriter(sw);
+
+        //    jsonSerializer.Serialize(jsonWriter, data);
+
+        //    jsonWriter.Close();
+        //    sw.Close();
+        //}
     }
 }
--- PWTestApp1 - ProposalMockup/Views/SavedAnnouncements.xaml.cs
+++ PWTestApp1 - ProposalMockup/Views/SavedAnnouncements.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +8,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
 
 namespace PWTestApp1___ProposalMockup.Views
 {
@@ -16,10 +19,21 @@ namespace PWTestApp1___ProposalMockup.Views
     {
         public string AnnouncementTitle;
         public string Content;
+        public string localPath;
+        public List<string> title = new List<string>();
         public SavedAnnouncements()
         {
             InitializeComponent();
-            
+            localPath = Path.Combine(FileSystem.AppDataDirectory, "AnnoucementTitle");
+            foreach (var i in localPath)
+            {
+                string a = File.ReadAllText(localPath);
+                title.Add(a);
+            }
+            foreach (var i in title)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
